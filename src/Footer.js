eb33cd983bad02b11e26c771c4704925a5455727
Maryam <maryam.timorrian@gmail.com>
2024-09-27 19:37:35 +0430

    starting of the first steps

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Your Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+// Main.js
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>This is where the main content goes.</p>
+    </main>
+  );
+}
+
+export default Main;
